073cbafebe16f873449f8a416017bbf8135f9b35
SirVoRaK <sirvoraky@gmail.com>
2023-07-04 11:42:24 -0300

    feat: flash nvim

--- lua/vorak/lazy.lua
+++ lua/vorak/lazy.lua
@@ -120,6 +120,54 @@ require("lazy").setup({
                 "nvim-telescope/telescope.nvim",
             },
         },
+        {
+            "folke/flash.nvim",
+            event = "VeryLazy",
+            opts = {},
+            keys = {
+                {
+                    "m",
+                    mode = { "n", "x", "o" },
+                    function()
+                        require("flash").jump({
+                                search = {
+                                    mode = function(str)
+                                        return "\\<" .. str
+                                    end,
+                                },
+                            })
+                    end,
+                    desc = "Flash",
+                },
+                {
+                    "M",
+                    mode = { "n", "o", "x" },
+                    function()
+                        require("flash").treesitter()
+                    end,
+                    desc = "Flash Treesitter",
+                },
+                {
+                    "r",
+                    mode = "o",
+                    function()
+                        require("flash").remote()
+                    end,
+                    desc = "Remote Flash",
+                },
+                {
+                    "R",
+                    mode = { "o", "x" },
+                    function()
+                        require("flash").treesitter_search()
+                    end,
+                    desc = "Flash Treesitter Search",
+                },
+            },
+        },
+        {
+            "folke/zen-mode.nvim",
+        },
 
         -- THEMES
         {
